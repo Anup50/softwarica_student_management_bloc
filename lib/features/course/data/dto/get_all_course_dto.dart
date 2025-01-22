9ac4cc2d2f3d5c4ce804276ec16a21cfbde8c5ea
Anup50 <kca3734@gmail.com>
2025-01-22 12:46:03 +0545

    first commit

---
+++ lib/features/course/data/dto/get_all_course_dto.dart
@@ -0,0 +1,17 @@
+import 'package:json_annotation/json_annotation.dart';
+import 'package:softwarica_student_management_bloc/features/course/data/model/course_api_model.dart';
+
+part 'get_all_course_dto.g.dart';
+
+@JsonSerializable()
+class GetAllCourseDTO {
+  final bool success;
+  final int count;
+  final List<CourseApiModel> data;
+
+  GetAllCourseDTO({
+    required this.success,
+    required this.count,
+    required this.data,
+  });
+}
